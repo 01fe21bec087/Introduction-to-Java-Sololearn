18ede9a8e8391b670ce151cc4ad57cea2aa5ad31
Abhinav Pawar <105110159+01fe21bec087@users.noreply.github.com>
2023-05-04 22:17:04 +0530

    Create That's odd.java

---
+++ That's odd.java
@@ -0,0 +1,49 @@
+/* 
+Author: Abhinav Pawar
+Date: 04-05-2023
+*/
+/* 
+You want to take a list of numbers and find the sum of all of the even numbers in the list. Ignore any odd numbers.
+Task:
+Find the sum of all even integers in a list of numbers.
+Input Format: 
+The first input denotes the length of the list (N). The next N lines contain the list elements as integers.
+Output Format: 
+An integer that represents the sum of only the even numbers in the list.
+Sample Input: 
+9
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+Sample Output: 
+20
+
+Explanation: 
+If you add together 2, 4, 6, and 8 from the list, you get a sum of 20.
+*/
+
+import java.util.Scanner;
+
+public class Program
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] num = new int[n];
+        int sum=0;
+        for(int i=0; i<n; i++)
+        {
+            num[i] = sc.nextInt();
+            if(num[i]%2 == 0)
+                sum += num[i];
+        }
+        System.out.println(sum);
+    }
+}
